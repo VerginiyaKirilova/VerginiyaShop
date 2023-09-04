f1b227b9a44b67f040876137355d68fe09125490
Verginiya Kirilova <verginiqkirilova@gmail.com>
2023-09-04 18:38:12 +0300

    first commit

---
+++ src/main/java/com/example/VerginiyaShop/VerginiyaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.VerginiyaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerginiyaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerginiyaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VerginiyaShop/VerginiyaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VerginiyaShop;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerginiyaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
