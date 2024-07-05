20435179ca9a7c5294af1f8b14141f5a293c6f21
Verginiya Kirilova <verginiqkirilova@gmail.com>
2024-07-05 14:08:38 +0300

    added country and states in menu

--- VerginiyaShopParent/VerginiyaShopBackEnd/src/main/java/com/shopme/admin/controller/CountryRestController.java
+++ VerginiyaShopParent/VerginiyaShopBackEnd/src/main/java/com/shopme/admin/controller/CountryRestController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
